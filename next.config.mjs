542934e4492b57047514a20cab976f0f792861a4
Mark Baumann <kontakt@markb.de>
2025-10-23 00:32:13 +0200

    Fehler bzgl. PWA

--- next.config.mjs
+++ next.config.mjs
@@ -20,6 +20,25 @@ let nextConfig = {
 nextConfig = withPWA({
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
+    cacheOnFrontendNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    fallbacks: {
+        document: '/~offline',
+    },
+    runtimeCaching: [
+        {
+            urlPattern: /^\/$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'start-url',
+                expiration: {
+                    maxEntries: 1,
+                    maxAgeSeconds: 24 * 60 * 60 // 24 hours
+                }
+            }
+        }
+    ]
 })(nextConfig);
 
 export default nextConfig;
